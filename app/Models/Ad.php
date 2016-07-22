2f06080fa4245c22f4a3bfb3fdf066bbf8628626
wzulfikar <wzulfikar@ymail.com>
2016-07-23 00:50:48 +0800

    added relationship for location

--- app/Models/Ad.php
+++ app/Models/Ad.php
@@ -2,9 +2,27 @@
 
 namespace App\Models;
 
+use App\Models\Map;
+use App\Models\User;
 use Illuminate\Database\Eloquent\Model;
 
 class Ad extends Model
 {
-    //
+	/**
+	 * Get ad's map.
+	 */
+	public function locations()
+	{
+		return $this->morphMany(Location::class, 'locatable');
+	}
+
+	/**
+	 * Get owning user
+	 * 
+	 * @return App\Models\User
+	 */
+	public function user()
+	{
+		return $this->belongsTo(User::class);
+	}
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -2,25 +2,35 @@
 
 namespace App\Models;
 
+use App\Models\Map;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
 class User extends Authenticatable
 {
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array
-     */
-    protected $fillable = [
-        'name', 'email', 'password',
-    ];
+  /**
+   * The attributes that are mass assignable.
+   *
+   * @var array
+   */
+  protected $fillable = [
+    'name', 'email', 'password',
+  ];
 
-    /**
-     * The attributes that should be hidden for arrays.
-     *
-     * @var array
-     */
-    protected $hidden = [
-        'password', 'remember_token',
-    ];
+  /**
+   * The attributes that should be hidden for arrays.
+   *
+   * @var array
+   */
+  protected $hidden = [
+    'password', 'remember_token',
+  ];
+
+  /**
+   * Get person's map.
+   * A person should've been in one place at any given time.
+   */
+  public function location()
+  {
+    return $this->morphMany(Location::class, 'locatable')->first();
+  }
 }
