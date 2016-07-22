5c2915a639a94c5544a58e1cfd5463565877eb2b
wzulfikar <wzulfikar@ymail.com>
2016-07-23 00:50:54 +0800

    added factory

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -1,5 +1,9 @@
 <?php
 
+use App\Models\Ad;
+use App\Models\User;
+use App\Models\Location;
+
 /*
 |--------------------------------------------------------------------------
 | Model Factories
@@ -11,11 +15,33 @@
 |
 */
 
-$factory->define(App\User::class, function (Faker\Generator $faker) {
-    return [
-        'name' => $faker->name,
-        'email' => $faker->safeEmail,
-        'password' => bcrypt(str_random(10)),
-        'remember_token' => str_random(10),
-    ];
+$factory->define(User::class, function (Faker\Generator $faker) {
+  return [
+    'name' => $faker->name,
+    'email' => $faker->safeEmail,
+    'password' => bcrypt(str_random(10)),
+    'remember_token' => str_random(10),
+  ];
 });
+
+$factory->define(Ad::class, function (Faker\Generator $faker) {
+  return [
+    'title' => $faker->catchPhrase,
+    'desc'  => sprintf('RM %s', $faker->numberBetween(400, 600)),
+  ];
+});
+
+$factory->defineAs(Location::class, 'usm-nearby', function (Faker\Generator $faker) {
+
+	$ad = factory(Ad::class)->create();
+
+  return [
+  	// lat & lng for area around USM
+		'lat' => $faker->latitude(5.3, 5.4),			
+		'lng' => $faker->longitude(100.2, 100.3),
+		'mappable_id' => $ad->id,
+		'mappable_type' => get_class($ad)
+  ];
+});
+
+
