be0d499da63e237db925a283368bac84f6d1951f
wzulfikar <wzulfikar@ymail.com>
2016-07-23 03:00:16 +0800

    eager load locatable

--- app/Lib/Traits/MySQLNearbyQueryBuilder.php
+++ app/Lib/Traits/MySQLNearbyQueryBuilder.php
@@ -2,21 +2,23 @@
 
 namespace App\Lib\Traits;
 
+use App\Models\Location;
 use Illuminate\Support\Facades\DB;
 
 trait MySQLNearbyQueryBuilder {
-  public function buildNearbyQuery($tbl, $rad_unit, $lat, $lng, $rad)
+  public function buildNearbyQuery($tbl, $measurement_unit, $lat, $lng, $rad)
   {
-    return DB::table($tbl)
-             ->select(DB::raw(
-              '*, ' . $rad_unit . ' * acos (
-                cos ( radians(78.3232) )
-                * cos( radians( lat ) )
-                * cos( radians( lng ) - radians(65.3234) )
-                + sin ( radians(78.3232) )
-                * sin( radians( lat ) )
-              ) AS distance'))
-             ->having('distance', '<=', $rad)
-             ->orderBy('distance');
+    // https://developers.google.com/maps/articles/phpsqlsearch_v3#findnearsql
+    return Location::with('locatable')
+                    ->select(DB::raw(
+                    'id, lat, lng, locatable_id, locatable_type, 
+                    ROUND(' . $measurement_unit . ' * acos ( cos ( radians(' . $lat . ') )
+                      * cos( radians( lat ) )
+                      * cos( radians( lng ) - radians(' . $lng . ') )
+                      + sin ( radians('  . $lat . ') )
+                      * sin( radians( lat ) )
+                    ), 2) AS distance'))
+                   ->having('distance', '<=', $rad)
+                   ->orderBy('distance');
   }
 }
